73f65f6314a4024a53226336e0ef63a3c5659359
christiandivincenzo <christiandivo98@gmail.com>
2020-12-12 17:07:49 -0500

    fixed

--- src/main/java/com/example/myapp2/repositories/PlaylistListenerRepository.java
+++ src/main/java/com/example/myapp2/repositories/PlaylistListenerRepository.java
@@ -2,7 +2,11 @@ package com.example.myapp2.repositories;
 
 import com.example.myapp2.models.PlaylistListener;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+@Repository
+@Service
 public interface PlaylistListenerRepository extends CrudRepository<PlaylistListener, Integer> {
 }
 
